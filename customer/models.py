2a3c39404f181f5d7290743e26e874020fa1fe91
howard.hsu <howard.hsu@drsays.org>
2023-02-25 23:46:31 +0800

    make customer addr tel cel nullto blank

--- customer/models.py
+++ customer/models.py
@@ -24,9 +24,9 @@ class CustomerStatus(models.Model):
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20)
-    address = models.CharField(max_length=50, null=True, blank=True)
+    address = models.CharField(max_length=50)
     email = models.EmailField(null=True, blank=True)
-    tel = models.CharField(max_length=20, null=True, blank=True)
+    tel = models.CharField(max_length=20)
     cel = models.CharField(max_length=20)
     line = models.CharField(max_length=20, null=True, blank=True)
     type = models.ForeignKey(CustomerType, on_delete=models.PROTECT, null=True, blank=True)
